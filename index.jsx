9fc04829951ef4eaf78ea5284a96c99fbda23313
Farras Abdulaziz El-Fahd <farraselfahd2000@gmail.com>
2024-10-02 21:13:48 +0700

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+// import style
+import './styles/style.css';
+
+// import component
+import App from './components/App';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
