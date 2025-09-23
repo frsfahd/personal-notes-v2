f3719d3741a3d4ab41616f5e1c6007881b45ad6c
Farras El-Fahd <farraselfahd2000@gmail.com>
2025-09-23 07:19:16 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = ({ handlerSearchAction }) => {
   };
   return (
     <header className="note-app__header">
-      <h1>Notes v2.1</h1>
+      <h1>Notes v2.2</h1>
       <form action="" className="note-search">
         <input onChange={handlerSearch} value={query} type="text" placeholder="cari catatan..." />
       </form>
